571b3e2579cac53d27f98a84092e8a30382cb4be
william arasirwa <mageboarasirwa03@gmail.com>
2023-04-23 18:05:32 +0300

    complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onDelete }) {
+  const displayTasks = tasks.map((task, index) => (
+    <Task
+      key={index}
+      text={task.text}
+      category={task.category}
+      onDelete={onDelete}
+    />
+  ));
+
+  return <div className="tasks">{displayTasks}</div>;
 }
 
 export default TaskList;
